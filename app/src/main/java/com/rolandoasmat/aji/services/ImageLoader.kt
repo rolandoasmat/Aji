e91b1f6240a59eed5d70b385048291f72bbbe2e8
Rolando Asmat <rolandoasmat@gmail.com>
2019-09-02 18:22:13 -0500

    Added mock appetizers list

--- app/src/main/java/com/rolandoasmat/aji/services/ImageLoader.kt
+++ app/src/main/java/com/rolandoasmat/aji/services/ImageLoader.kt
@@ -1,10 +1,15 @@
 package com.rolandoasmat.aji.services
 
 import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 object ImageLoader : ImageLoaderService {
 
     override fun load(url: String, imageView: ImageView) {
-        // Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+         Picasso
+             .get()
+             .load(url)
+             .resize(600, 400)
+             .into(imageView)
     }
 }
--- app/src/main/java/com/rolandoasmat/aji/ui/appetizers/AppetizersViewModel.kt
+++ app/src/main/java/com/rolandoasmat/aji/ui/appetizers/AppetizersViewModel.kt
@@ -14,9 +14,11 @@ class AppetizersViewModel : ViewModel() {
      */
     fun fetch() {
         // TODO fetch from network
-        val ceviche = MealsListItemUiModel("", "Ceviche")
-        val anticuchos = MealsListItemUiModel("", "Anticucho")
-        val meals = listOf(ceviche, anticuchos)
+        val ceviche = MealsListItemUiModel("https://cevicheperuano.net/wp-content/uploads/2018/12/ceviche-de-pescado_700x465.jpg", "Ceviche")
+        val anticuchos = MealsListItemUiModel("https://e.rpp-noticias.io/normal/2018/11/12/252825_709013.jpg", "Anticuchos")
+        val papaHuancaina = MealsListItemUiModel("https://www.whats4eats.com/files/vegetables-papa-huancaina-wikimedia-AgainErick-4x3.jpg", "Papa a la Huancaina")
+        val causa = MealsListItemUiModel("https://www.seriouseats.com/2018/07/20180702-peruvian-causa-vicky-wasik-21-625x469.jpg", "Causa")
+        val meals = listOf(ceviche, anticuchos, papaHuancaina, causa)
         val mockResponse = MealsListUiModel(meals)
         this.meals.value = mockResponse
     }
