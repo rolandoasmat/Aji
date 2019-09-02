ff7f135c93136b05dcee8edb6fa1acfe9e55b413
Rolando Asmat <rolandoasmat@gmail.com>
2019-09-02 12:14:31 -0500

    Added meals list custom view

--- app/src/main/java/com/rolandoasmat/aji/mealslist/MealsListAdapter.kt
+++ app/src/main/java/com/rolandoasmat/aji/mealslist/MealsListAdapter.kt
@@ -1,38 +1,56 @@
 package com.rolandoasmat.aji.mealslist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.rolandoasmat.aji.R
+import com.rolandoasmat.aji.services.ImageLoader
+import kotlinx.android.synthetic.main.cell_item_meals_list.view.*
 
 /**
  * Adapter for showing a list of meals
  */
+private typealias MealsList = List<MealsListItemUiModel>
+
 class MealsListAdapter : RecyclerView.Adapter<MealsListAdapter.ViewHolder>() {
 
-    private var data: MealsListUiModel? = null
+    private var data: MealsList? = null
 
-    fun setData(data: MealsListUiModel) {
+    fun setData(data: MealsList) {
         this.data = data
+        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val context = parent.context
+        val layoutID = R.layout.cell_item_meals_list
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(layoutID, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int = data?.meals?.size ?: 0
+    override fun getItemCount(): Int = data?.size ?: 0
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        data?.meals?.getOrNull(position)?.let {
+        data?.getOrNull(position)?.let {
             holder.bind(it)
         }
     }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
+        private val mealBackdrop: ImageView? = view.mealBackdrop
+        private val mealTitle: TextView? = view.mealTitle
+
         fun bind(data: MealsListItemUiModel) {
-
+            mealBackdrop?.let {
+                ImageLoader.load(data.backdropURL, it)
+            }
+            mealTitle?.text = data.mealTitle
         }
-
     }
 
 }
--- app/src/main/java/com/rolandoasmat/aji/mealslist/MealsListView.kt
+++ app/src/main/java/com/rolandoasmat/aji/mealslist/MealsListView.kt
@@ -1,2 +1,28 @@
 package com.rolandoasmat.aji.mealslist
 
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Custom view that encapsulates a list
+ * of meals
+ */
+class MealsListView(context: Context, attrs: AttributeSet) : RecyclerView(context, attrs) {
+
+    private val mealsAdapter: MealsListAdapter = MealsListAdapter()
+
+    init {
+        layoutManager = LinearLayoutManager(context)
+        adapter = mealsAdapter
+    }
+
+    /**
+     * Set the list of meals data
+     */
+    fun setData(data: MealsListUiModel) {
+        mealsAdapter.setData(data.meals)
+
+    }
+}
