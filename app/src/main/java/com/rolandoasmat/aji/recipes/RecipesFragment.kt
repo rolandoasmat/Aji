82b609e1da19719cce374005f5363eb855593ab4
Rolando Asmat <rolandoasmat@gmail.com>
2020-08-05 22:39:14 -0400

    Nav graph updates, title copy change

--- app/src/main/java/com/rolandoasmat/aji/recipes/RecipesFragment.kt
+++ app/src/main/java/com/rolandoasmat/aji/recipes/RecipesFragment.kt
@@ -1,9 +1,11 @@
 package com.rolandoasmat.aji.recipes
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
