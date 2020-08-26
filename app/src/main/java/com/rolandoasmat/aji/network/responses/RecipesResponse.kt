c82c0f32ad8ae740019b88f805f935163a7241f0
Rolando Asmat <rolandoasmat@gmail.com>
2020-08-25 23:35:29 -0400

    Deleted response file

--- app/src/main/java/com/rolandoasmat/aji/network/responses/RecipesResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.rolandoasmat.aji.network.responses
-
-data class RecipesResponse(val entries: List<Entry>) {
-    data class Entry(val title: String,
-                     val description: String)
-}
