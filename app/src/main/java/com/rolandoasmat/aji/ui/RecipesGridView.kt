3c3d4cb398e3f843e3209b7a2d653bb65fa84b07
Rolando Asmat <rolandoasmat@gmail.com>
2021-02-22 21:43:20 -0600

    Progress on generic grid view

--- app/src/main/java/com/rolandoasmat/aji/ui/RecipesGridView.kt
+++ app/src/main/java/com/rolandoasmat/aji/ui/RecipesGridView.kt
@@ -1,14 +1,28 @@
 package com.rolandoasmat.aji.ui
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.core.view.forEach
 
-class RecipesGridView(context: Context) : LinearLayout(context) {
+class RecipesGridView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs), RecipesRecyclerView.Callbacks {
 
 
+    init {
+        orientation = VERTICAL
+    }
 
     fun setUiModel(uiModel: RecipesGridViewUiModel) {
+        this.removeAllViews()
+        uiModel.sections.forEach { section ->
+            val sectionView = RecipeSectionView(context, section, this)
+            addView(sectionView)
 
+        }
+    }
+
+    override fun onImageTapped(recipeID: String) {
+        // TODO
     }
 
 }
