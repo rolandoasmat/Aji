d081a8a39ef8e4226c2e86a38e787cdfafeac43d
Rolando Asmat <rolandoasmat@gmail.com>
2021-04-26 21:57:33 -0500

    Updated repo to use extension functions

--- client/android/app/src/main/java/com/rolandoasmat/aji/repositories/RecipesRepository.kt
+++ client/android/app/src/main/java/com/rolandoasmat/aji/repositories/RecipesRepository.kt
@@ -11,7 +11,6 @@ import com.rolandoasmat.aji.network.Resource
 import com.rolandoasmat.aji.db.FavoriteRecipeEntity
 import com.rolandoasmat.aji.model.*
 import com.rolandoasmat.aji.network.AjiApolloClient
-import java.lang.IllegalStateException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,21 +20,16 @@ class RecipesRepository @Inject constructor(
     private val databaseRepository: DatabaseRepository,
     private val ajiApolloClient: AjiApolloClient) {
 
-    fun fetchRecipes(): LiveData<Resource<List<Recipe>>> {
-        return object : NetworkBoundResource<List<Recipe>, ListRecipesQuery.Data>(coroutineContextProvider) {
+    fun fetchRecipes(): LiveData<Resource<Recipes>> {
+        return object : NetworkBoundResource<Recipes, ListRecipesQuery.Data>(coroutineContextProvider) {
             override fun createCall(): ApolloQueryCall<ListRecipesQuery.Data> {
                 val query = ListRecipesQuery()
                 return ajiApolloClient.query(query)
             }
 
-            override fun processResponse(response: ApiSuccessResponse<ListRecipesQuery.Data>): List<Recipe> {
-                return response.body.getRecipes?.fragments?.recipesFragment?.items?.map { item ->
-                    item.fragments.recipeGroupFragment.recipes.map { recipe ->
-                        recipe.fragments.recipeFragment.toRecipe()
-                    }
-                } ?: emptyList()
+            override fun processResponse(response: ApiSuccessResponse<ListRecipesQuery.Data>): Recipes {
+                return response.body.getRecipes!!.fragments.recipesFragment.toRecipes()
             }
-
         }.asLiveData()
     }
 
@@ -47,24 +41,7 @@ class RecipesRepository @Inject constructor(
             }
 
             override fun processResponse(response: ApiSuccessResponse<GetRecipeDetailsQuery.Data>): RecipeDetails {
-                val recipeDetails = response.body.recipeDetails ?: throw IllegalStateException("Empty recipe details")
-                val ingredients = mutableListOf<Ingredient>()
-                recipeDetails.ingredients()?.forEach {
-                    it?.let {
-                        val ingredient = Ingredient(it.name(), it.amount() ?: "") // TODO remove once schema updated
-                        ingredients.add(ingredient)
-                    }
-                }
-                val steps = recipeDetails.steps().map { Step(it) }
-                return RecipeDetails(
-                    recipeDetails.id(),
-                    recipeDetails.title() ?: "",
-                    recipeDetails.imageURL() ?: "", // TODO remove once schema updated
-                    recipeDetails.description() ?: "",
-                    recipeDetails.duration() ?: "", // TODO remove once schema updated
-                    recipeDetails.servings() ?: "", // TODO remove once schema updated
-                    ingredients,
-                    steps)
+                return response.body.getRecipeDetails!!.fragments.recipeDetailsFragment.toRecipeDetails()
             }
         }.asLiveData()
     }
