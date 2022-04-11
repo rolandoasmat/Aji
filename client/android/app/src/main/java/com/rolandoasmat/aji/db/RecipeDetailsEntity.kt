80907c9c84c8c5a7771d58685ed0043683ce5c9b
Rolando Asmat <rolandoasmat@gmail.com>
2022-04-11 01:55:59 -0500

    Created empty recipe details entity. Added TODOs

---
+++ client/android/app/src/main/java/com/rolandoasmat/aji/db/RecipeDetailsEntity.kt
@@ -0,0 +1,5 @@
+package com.rolandoasmat.aji.db
+
+// TODO RAA create entity to store recipe details data: info, ingredients, steps
+class RecipeDetailsEntity {
+}
--- client/android/app/src/main/java/com/rolandoasmat/aji/recipe_breakdown/RecipeBreakdownFragment.kt
+++ client/android/app/src/main/java/com/rolandoasmat/aji/recipe_breakdown/RecipeBreakdownFragment.kt
@@ -10,7 +10,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.rolandoasmat.aji.R
 import com.rolandoasmat.aji.model.RecipeBreakdownStep
 import kotlinx.android.synthetic.main.fragment_recipe_breakdown.*
-
+// TODO RAA add DI to this screen as well as support landscape mode
 /**
  * Step by step instructions of the fragment
  */
